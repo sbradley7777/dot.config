a8fb314174249dfdf2c3fed14957ccca5418f8c1
shane bradley <shanebradley@gmail.com>
2013-08-05 09:55:06 -0400

    Remove symbolic links on install and add symbolic links back so that path is correct.

--- install.sh
+++ install.sh
@@ -1,27 +1,40 @@
 #!/bin/sh
+# If the files are not symbolic links then they will be backed up. If the files
+# are symbolic links then they will be removed and readded so that paths are
+# correct.
+
 echo "Installing configs for $(hostname)";
 
 if [  ! -L ~/.bash_profile ]; then
     mv -f ~/.bash_profile ~/.bash_profile.org > /dev/null 2>&1;
-    ln -s ~/github/dot.config/bash/.bash_profile ~/.bash_profile;
+else
+    rm -f ~/.bash_profile
 fi
+ln -s ~/github/dot.config/bash/.bash_profile ~/.bash_profile;
+
 if [  ! -L ~/.bashrc ]; then
     mv -f ~/.bashrc ~/.bashrc.org > /dev/null 2>&1;
-    ln -s ~/github/dot.config/bash/.bashrc ~/.bashrc;
+else
+    rm -f ~/.bashrc
 fi
+ln -s ~/github/dot.config/bash/.bashrc ~/.bashrc;
 
 touch ~/.bash_profile.priv;
 touch ~/.bashrc.priv;
 
 if [  ! -L ~/.emacs ]; then
     mv -f ~/.emacs ~/.emacs.org > /dev/null 2>&1;
-    ln -s ~/github/dot.config/emacs.d/dot.emacs.el ~/.emacs;
+else
+    rm -f ~/.emacs
 fi
+ln -s ~/github/dot.config/emacs.d/dot.emacs.el ~/.emacs;
 
 if [  ! -L ~/.emacs.d ]; then
     mv -f ~/.emacs.d ~/.emacs.org > /dev/null 2>&1;
-    ln -s ~/github/dot.config/emacs.d/ ~/.emacs.d;
+else
+    rm -f ~/.emacs.d
 fi
+ln -s ~/github/dot.config/emacs.d/ ~/.emacs.d;
 
 echo "Please relogin for configurations to be reloaded.";
 exit 0;

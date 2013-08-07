d4f4fe5ee6615750a6c15d85a228b3e7561cc123
shane bradley <shanebradley@gmail.com>
2013-08-07 09:36:13 -0400

    Added a new version function and update theme for versions older than 22

--- emacs.d/site-lisp/my-functions.el
+++ emacs.d/site-lisp/my-functions.el
@@ -2,7 +2,7 @@
 ;; Accept y or n Instead of yes or no:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun yes-or-no-p (arg)
-  "An alias for y-or-n-p, because I hate having to tye 'yes' or 'no'."
+  "An alias for y-or-n-p, because I hate having to type 'yes' or 'no'."
  (y-or-n-p arg))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -26,3 +26,17 @@
       (byte-compile-file (buffer-file-name))))
 (add-hook 'after-save-hook 'autocompile)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Older versions of emacs did not have these variables
+;; (emacs-major-version and emacs-minor-version.)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(if (and (not (boundp 'emacs-major-version))
+         (string-match "^[0-9]+" emacs-version))
+    (setq emacs-major-version
+          (string-to-int (substring emacs-version
+                                    (match-beginning 0) (match-end 0)))))
+(if (and (not (boundp 'emacs-minor-version))
+         (string-match "^[0-9]+\\.\\([0-9]+\\)" emacs-version))
+    (setq emacs-minor-version
+          (string-to-int (substring emacs-version
+                                    (match-beginning 1) (match-end 1)))))
--- emacs.d/site-lisp/my-theme.el
+++ emacs.d/site-lisp/my-theme.el
@@ -16,40 +16,48 @@
      :foreground "red")
     )))
 
-(set-background-color        "red") ; Set emacs bg color 
-(set-foreground-color        "white") ; Set emacs fg color 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Detect the version of emacs that is running and operating system:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; The version information for various Operating Systems by doing
-;; "meta-x version on scratch buffer"
-;;
-;; Here is a list of version strings:
-;; Fedora 14:
-;; GNU Emacs 23.2.1 (x86_64-redhat-linux-gnu, GTK+ Version 2.21.4) of 2010-07-08 on x86-10.phx2.fedoraproject.org
-;; RHEL 5:
-;; GNU Emacs 21.4.1 (x86_64-redhat-linux-gnu, X toolkit, Xaw3d scroll bars) of 2011-04-07 on hs20-bc2-3.build.redhat.com
-;; RHEL 6:
-;; GNU Emacs 23.1.1 (x86_64-redhat-linux-gnu, GTK+ Version 2.18.9) of 2011-02-01 on x86-003.build.bos.redhat.com
-;; OSX Lion 10.7.1
-;; GNU Emacs 22.1.1 (mac-apple-darwin) of 2011-06-24 on doublemagnum.apple.com
+(set-background-color "red")
+(set-foreground-color "white")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Detect the version of emacs that is running:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Certain versions of emacs do not have all the functions like GNU
 ;; Emacs 21. We will detect the version and then load functions that
 ;; apply to each version so that we do not error out on loading.
 (cond
- ((string-match "GNU Emacs 21" (emacs-version))
-  (message "Detected that emacs is version: GNU Emacs 21.")
+ ((< emacs-major-version 22)
+  (message "Detected that emacs is version: %s." emacs-version)
   ;; Put in version releated font preferences in here.
-  )
- ((string-match "GNU Emacs 22" (emacs-version))
-  (message "Detected that emacs is version: GNU Emacs 22.")
-  ;; Changes the mode line colors for background.
-  (set-face-background 'modeline "black")
-  ;; Changes the mode line colors for foreground.
-  (set-face-foreground 'modeline "orange")
-  ;; Highlight color you see when you mark text with the mouse
-  (set-face-foreground 'region "white")
+  (setq
+   font-lock-face-attributes
+   '((font-lock-comment-face "Red" nil nil t nil)
+     (font-lock-string-face "Gold" nil nil t nil)
+     (font-lock-keyword-face "MediumAquamarine")
+     (font-lock-function-name-face "Yellow")
+     (font-lock-variable-name-face "Green")
+     (font-lock-type-face "Pink")
+     (font-lock-reference-face "Red" nil nil t nil))
+   font-lock-maximum-decoration t)
+   ;;(let (new-face)
+   ;;  (setq new-face (copy-face 'italic 'firebrick-italic))
+   ;;  (set-face-foreground new-face "firebrick")
+   ;;  (setq new-face (copy-face 'bold 'blue))
+   ;;  (set-face-foreground new-face "blue")
+   ;;  (setq new-face (copy-face 'bold 'royalblue))
+   ;;  (set-face-foreground new-face "royalblue")
+   ;;  (setq new-face (copy-face 'bold 'yellow))
+   ;;  (set-face-foreground new-face "yellow")
+   ;;  (setq new-face (copy-face 'bold 'firebrick))
+   ;;  (set-face-foreground new-face "firebrick")
+   ;;  (setq
+   ;;   font-lock-comment-face 'firebrick-italic
+   ;;   font-lock-string-face 'italic
+   ;;   font-lock-keyword-face 'blue
+   ;;   font-lock-function-name-face 'yellow
+   ;;   font-lock-variable-name-face 'black
+   ;;   font-lock-type-face 'royalblue
+   ;;   font-lock-reference-face 'firebrick)))
   )
  (t (message "Detected that emacs is not version: GNU Emacs 21.")
     ;; Font for keywords, self, etc.
@@ -72,7 +80,7 @@
     (set-face-foreground 'font-lock-variable-name-face "Green")
     (custom-set-faces)
     )
- )
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load font lock mode

8c65237ed5e9dab01c969e271236e4103f208620
shane bradley <shanebradley@gmail.com>
2013-09-24 11:21:25 -0400

    Changes and options added to fsync_files.cc

--- bin/bin.clusterha_probe/fsync_files.cc
+++ bin/bin.clusterha_probe/fsync_files.cc
@@ -9,6 +9,12 @@
 // both files are compared. Returns 1 is files are identical and not 1 if there
 // is error or not identical.
 //
+// NOTE: The parent directory will be fsync() after the file has had fsync()
+// executed. fsync() on parent directory does have a performance penalty cause
+// this will run a lot slower in my tests. Results will vary. Normally the parent
+// directory would only by fsync() after writing has completed and not after
+// every write.
+//
 // The maximum number of iterations in the loop ca n be set by changing the
 // variable: MAXIMUM_INTEGER. Which is the last integer that will be written.
 //
@@ -16,16 +22,30 @@
 //        ./fsync_files /mnt/gfs1vol1 /mnt/ext3vol1;
 // Compile: g++ -o fsync_files fsync_files.cc
 
+// TODO:
+// - Add option parser for options: path1, path2, max_int, int_print,
+//   fsync_parent_enabled.
+// - Add fsync() on parent directory of both paths after last write.
+
 #include <iostream>
 #include <fstream>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <dirent.h>
 using namespace std;
 
 // The maximum integer to write to the file.
-//int MAXIMUM_INTEGER = 10000000;
 int MAXIMUM_INTEGER = 1000000;
+//int MAXIMUM_INTEGER = 10000000;
+
+// An integer that will be used for printing every X integer. For example, print
+// integer after every 10000 complete.
+int INTEGER_PRINT = 1000;
+
+// IF this int is 1 then an fsync() on parent directory after ever write will be
+// done.
+int FSYNC_PARENT_AFTER_WRITE = 1;
 
 u_int32_t get_file_size(const char *path_to_file) {
   if(access(path_to_file, F_OK ) != -1 ) {
@@ -52,16 +72,25 @@ int main (int argc, char* argv[]) {
     return -1;
   }
 
-  // Check if mountpoints exists
+  // Create path1 and check that it is valid.
   string path1 = argv[1];
   if ((path1.length() > 0) && (path1[path1.length() - 1] == '/')) {
     path1 = path1.substr(0, path1.length() - 1);
   }
-
   if(access(path1.c_str(), F_OK ) == -1 ) {
     printf("ERROR: The path to mount point 1 does not exists or cannnot be accessed: %s.\n", path1.c_str());
     return -1;
   }
+  DIR *dp1 = NULL;
+  if (FSYNC_PARENT_AFTER_WRITE) {
+    if((dp1 = opendir(path1.c_str())) == NULL) {
+      printf("ERROR: Cannot open path 1 directory: %s\n", path1.c_str());
+      return -1;
+    }
+  }
+  path1 += "/fsync_test.txt";
+
+  // Create path2 and check that it is valid.
   string path2 = argv[2];
   if ((path2.length() > 0) && (path2[path2.length() - 1] == '/')) {
     path2 = path2.substr(0, path2.length() - 1);
@@ -70,9 +99,13 @@ int main (int argc, char* argv[]) {
     printf("ERROR: The path to mount point 2 does not exists or cannnot be accessed: %s.\n", path2.c_str());
     return -1;
   }
-
-  // Mount points exist, so file paths strings will be created.
-  path1 += "/fsync_test.txt";
+  DIR *dp2 = NULL;
+  if (FSYNC_PARENT_AFTER_WRITE) {
+    if((dp2 = opendir(path2.c_str())) == NULL) {
+      printf("ERROR: Cannot open path 2 directory: %s\n", path2.c_str());
+      return -1;
+    }
+  }
   path2 += "/fsync_test.txt";
 
   // Remove any exisiting files.
@@ -97,6 +130,7 @@ int main (int argc, char* argv[]) {
   // Write the integers to the files.
   while (i <= MAXIMUM_INTEGER) {
     fprintf(file1, "%lu\n", i);
+    // fsync file1 data after writing it.
     rc = fsync(fd1);
     if (rc != 0) {
       printf("WARNING: Sync failed on file 1: %s.", path1.c_str());
@@ -104,8 +138,18 @@ int main (int argc, char* argv[]) {
       fclose(file2);
       return -1;
     }
-
+    // fsync the parent directory to make sure data is written to disk.
+    if (dp1 != NULL) {
+      int dpfd1 = dirfd(dp1);
+      rc = fsync(dpfd1);
+      if (rc != 0) {
+        printf("ERROR: Sync failed on parent directory for the file : %s.\n", path1.c_str());
+        return -1;
+      }
+    }
+    // fsync file2 data after writing it.
     fprintf(file2, "%lu\n", i);
+    // fsync file2 data.
     rc = fsync(fd2);
     if (rc != 0) {
       printf("WARNING: Sync failed on file 2: %s.", path2.c_str());
@@ -113,8 +157,18 @@ int main (int argc, char* argv[]) {
       fclose(file2);
       return -1;
     }
-    if ((i != 0) && (i%100000 == 0)) {
-      printf("The counter has wrote %d lines.\n", i);
+    // fsync the parent directory to make sure data is written to disk.
+    if (dp2 != NULL) {
+      int dpfd2 = dirfd(dp2);
+      rc = fsync(dpfd2);
+      if (rc != 0) {
+        printf("ERROR: Sync failed on parent directory for the file : %s.\n", path2.c_str());
+        return -1;
+      }
+    }
+
+    if ((i != 0) && (i%INTEGER_PRINT == 0)) {
+      printf("The counter has wrote %d/%d lines.\n", i, MAXIMUM_INTEGER);
     }
     i += 1;
   }
@@ -123,6 +177,12 @@ int main (int argc, char* argv[]) {
   // Close the files and exit.
   fclose(file1);
   fclose(file2);
+  if (dp1 != NULL) {
+    closedir(dp1);
+  }
+  if (dp2 != NULL) {
+    closedir(dp2);
+  }
 
   // Compare the sizes of both the files.
   u_int32_t file1_size = get_file_size(path1.c_str());

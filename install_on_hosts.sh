d58fd6dc63c544d22959633f26c18837ab991c75
Shane Bradley <sbradley@redhat.com>
2013-12-12 10:15:53 -0500

    Updated the multiple host installer to work with new python installer

--- install_on_hosts.sh
+++ install_on_hosts.sh
@@ -85,9 +85,8 @@ for i in ${!LIST_OF_HOSTS[@]}
                 if [ $VERBOSE ]; then
                     echo "The git repo $PATH_TO_GIT_REPO_DIR on $currentHostname does not exists and will be created.";
                 fi
-                # result=$(ssh $currentHostname  "mkdir -p $PATH_TO_GIT_REPO_DIR; git clone --quiet git://github.com/sbradley7777/dot.config.git $PATH_TO_GIT_REPO_DIR; $PATH_TO_GIT_REPO_DIR/install.sh;")$
-                result=$(ssh $currentHostname  "mkdir -p $PATH_TO_GIT_REPO_DIR; git clone --quiet git://github.com/sbradley7777/dot.config.git $PATH_TO_GIT_REPO_DIR; python $PATH_TO_GIT_REPO_DIR/install.py -y -q;")$
-                if [ -n "$result" ] ; then
+                ssh $currentHostname  "mkdir -p $PATH_TO_GIT_REPO_DIR; git clone --quiet git://github.com/sbradley7777/dot.config.git $PATH_TO_GIT_REPO_DIR; python $PATH_TO_GIT_REPO_DIR/install.py -y -q;";
+                if [ "$?" == "0" ] ; then
                     echo "$currentHostname: SUCCESS";
                 else
                     echo "$currentHostname: FAILED";
@@ -96,10 +95,8 @@ for i in ${!LIST_OF_HOSTS[@]}
                 if [ $VERBOSE ]; then
                     echo "The git repo $PATH_TO_GIT_REPO_DIR on $currentHostname does exists and will be updated.";
                 fi
-                # result=$(ssh $currentHostname  "cd $PATH_TO_GIT_REPO_DIR; git reset --quiet --hard origin/master; git pull --quiet; $PATH_TO_GIT_REPO_DIR/install.sh;");
-                result=$(ssh $currentHostname  "cd $PATH_TO_GIT_REPO_DIR; git reset --quiet --hard origin/master; git pull --quiet; python $PATH_TO_GIT_REPO_DIR/install.py -y -q;");
-                echo $result
-                if [ "$result" == "0" ] ; then
+                ssh $currentHostname  "cd $PATH_TO_GIT_REPO_DIR; git reset --quiet --hard origin/master; git pull --quiet; python $PATH_TO_GIT_REPO_DIR/install.py -y -q;"
+                if [ "$?" == "0" ] ; then
                     echo "$currentHostname: SUCCESS";
                 else
                     echo "$currentHostname: FAILED";

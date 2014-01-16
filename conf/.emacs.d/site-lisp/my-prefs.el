d5bbd285e92eaef0488e542a9c3c6a63b5df8bb2
Shane Bradley <shanebradley@gmail.com>
2014-01-16 09:47:22 -0500

    Added lisp code to emacs config to auto determine spaces or tabs and yes to follow symlinks.

--- conf/.emacs.d/site-lisp/my-prefs.el
+++ conf/.emacs.d/site-lisp/my-prefs.el
@@ -28,6 +28,8 @@
 
 ;; Turn off read only mode with .patch files
 (setq diff-default-read-only nil)
+;; Follow symlinks and don't ask.
+(setq vc-follow-symlinks t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Line Preferences:
@@ -59,10 +61,37 @@
 ;; $ reindent.py ~/somefile.py
 
 ;; Show tabs as 4 spaces
-(setq-default tab-width 4)
+;;(setq-default tab-width 4)
 ;; No tabs, just spaces are used
-(setq-default indent-tabs-mode nil)
+;;(setq-default indent-tabs-mode nil)
 
+
+(defun how-many-region (begin end regexp &optional interactive)
+  "Print number of non-trivial matches for REGEXP in region.
+Non-interactive arguments are Begin End Regexp"
+  (interactive "r\nsHow many matches for (regexp): \np")
+  (let ((count 0) opoint)
+    (save-excursion
+      (setq end (or end (point-max)))
+      (goto-char (or begin (point)))
+      (while (and (< (setq opoint (point)) end)
+                  (re-search-forward regexp end t))
+        (if (= opoint (point))
+            (forward-char 1)
+          (setq count (1+ count))))
+      (if interactive (message "%d occurrences" count))
+      count)))
+
+(defun infer-indentation-style ()
+  ;; if our source file uses tabs, we use tabs, if spaces spaces, and if
+  ;; neither, we use the current indent-tabs-mode
+  (let ((space-count (how-many-region (point-min) (point-max) "^  "))
+        (tab-count (how-many-region (point-min) (point-max) "^\t")))
+    (if (> space-count tab-count) (setq indent-tabs-mode nil))
+    (if (> tab-count space-count) (setq indent-tabs-mode t))))
+
+(setq indent-tabs-mode nil)
+(infer-indentation-style)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load slang mode support for event scripts in clustering:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

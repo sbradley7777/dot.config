5260482d1481880325df540d2bcb6f0ae6c1daba
Shane Bradley <shanebradley@gmail.com>
2014-05-08 10:14:00 -0400

    Validate IP given.

--- bin/bin.clusterha/net_breaker.sh
+++ bin/bin.clusterha/net_breaker.sh
@@ -13,12 +13,30 @@
 # Based on the following script:
 # - https://github.com/corosync/corosync/blob/master/cts/agents/net_breaker.sh
 ######################################################################
+is_valid_ip() {
+   if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
+   then
+      #echo "IP is $1"
+      ret=0
+   else
+      #echo "$1 is not IP"
+      ret=1
+   fi
+   return $ret
+}
+
 if [ -z $1 ]; then
   iptables -F >/dev/null 2>&1
 else
-  iptables -A INPUT -s $1 -j DROP >/dev/null 2>&1
-  iptables -A OUTPUT -s $1 -j DROP >/dev/null 2>&1
-  iptables -A INPUT -m pkttype --pkt-type multicast -j DROP
+    if is_valid_ip $1; then
+	iptables -A INPUT -s $1 -j DROP >/dev/null 2>&1
+	iptables -A OUTPUT -s $1 -j DROP >/dev/null 2>&1
+	iptables -A INPUT -m pkttype --pkt-type multicast -j DROP
+    else
+	echo "ERROR: The IP address is invalid: $1";
+	echo "Usage: net_breaker.sh <IP adddress>";
+	exit 1;
+    fi
 fi
 
 exit 0

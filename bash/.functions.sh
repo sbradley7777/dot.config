2de47a849ebdacc911f3a6b05a3687dc250e6a3a
Shane Bradley <shanebradley@gmail.com>
2014-11-04 10:06:02 -0500

    Updated my osx prefs.

--- bash/.functions.sh
+++ bash/.functions.sh
@@ -2,6 +2,29 @@
 # Functions that are for bash.
 # Some of thesee function are from here: https://github.com/mathiasbynens/dotfiles/blob/master/.functions
 
+# Set the colours you can use
+black='\033[0;30m'
+white='\033[0;37m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+
+#  Reset text attributes to normal + without clearing screen.
+alias Reset="tput sgr0"
+
+# Color-echo.
+# arg $1 = message
+# arg $2 = Color
+cecho() {
+  echo "${2}${1}"
+  Reset # Reset to normal.
+  return
+}
+
 # Check to see if command exists
 command_exists () {
     type "$1" &> /dev/null ;
--- bin/bin.osx/osx-for-hackers.sh
+++ bin/bin.osx/osx-for-hackers.sh
@@ -8,29 +8,13 @@
 # Yosemite hiddle utilies link: https://github.com/diimdeep/dotfiles/blob/master/osx/configure/yosemite/link_hidden_utilities
 # OSX Lion Tweaks: http://knoopx.net/2011/10/28/os-x-lion-tweaks
 
+# TODO:
+# * Add the osx-prefs.sh items to here.
+# * Test this code first
+# * Get rid of questions or set an auto yes in there option.
 
-# Set the colours you can use
-black='\033[0;30m'
-white='\033[0;37m'
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-blue='\033[0;34m'
-magenta='\033[0;35m'
-cyan='\033[0;36m'
-
-
-#  Reset text attributes to normal + without clearing screen.
-alias Reset="tput sgr0"
-
-# Color-echo.
-# arg $1 = message
-# arg $2 = Color
-cecho() {
-  echo "${2}${1}"
-  Reset # Reset to normal.
-  return
-}
+# Source in the color functions and colors
+source ~/.functions.sh;
 
 # Set continue to false by default
 CONTINUE=false
@@ -67,9 +51,373 @@ else
   ###############################################################################
   # General UI/UX
   ###############################################################################
+  echo ""
+  echo "Hide the Spotlight icon? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+        sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
+        break;;
+    *) break;;
+  esac
 
   echo ""
-  echo "Would you like to set your computer name (as done via System Preferences >> Sharing)?  (y/n)"
+  echo "Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+	  sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName;
+        break;;
+    *) break;;
+  esac
 
+  echo ""
+  echo "Check for software updates daily, not just once per week? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+	  defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1;
+        break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Removing duplicates in the 'Open With' menu? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+	  /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user;
+        break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Disable crash reporter dialog box? (y/n)"
+  read -r response
+  case $response in
+      defaults write com.apple.CrashReporter DialogType -string "none"
+      break;;
+      *) break;;
+  esac
+
+  echo ""
+  echo "Disable dashboard from starting on boot? (y/n)"
+  read -r response
+  case $response in
+      defaults write com.apple.dashboard mcx-disabled -boolean YES
+      break;;
+      *) break;;
+  esac
+
+  ###############################################################################
+  # Screen
+  ###############################################################################
+  echo ""
+  echo "Requiring password immediately after sleep or screen saver begins"
+  read -r response
+  case $response in
+    [yY])
+	  defaults write com.apple.screensaver askForPassword -int 1;
+	  defaults write com.apple.screensaver askForPasswordDelay -int 0;
+        break;;
+    *) break;;
+  esac
+
+  echo "";
+  echo "Where do you want screenshots to be stored? (hit ENTER if you want ~/Desktop as default)";
+  read screenshot_location
+  if [ -z "$1" ]
+  then
+    echo ""
+    echo "Setting location to ~/Desktop"
+    defaults write com.apple.screencapture location -string "$HOME/Desktop";
+  else
+    echo ""
+    echo "Setting location to ~/$screenshot_location"
+    defaults write com.apple.screencapture location -string "$HOME/$screenshot_location"
+  fi
+
+  echo ""
+  echo "What format should screenshots be saved as? (hit ENTER for PNG, options: BMP, GIF, JPG, PDF, TIFF) "
+  read screenshot_format
+  if [ -z "$1" ]
+  then
+    echo ""
+    echo "Setting screenshot format to PNG"
+    defaults write com.apple.screencapture type -string "png"
+  else
+    echo ""
+    echo "Setting screenshot format to $screenshot_format"
+    defaults write com.apple.screencapture type -string "$screenshot_format"
+  fi
+
+  ###############################################################################
+  # Finder
+  ###############################################################################
+  echo ""
+  echo "Do not hid the ~/Library directory? (y/n)"
+  case $response in
+    [yY])
+	  chflags nohidden ~/Library;
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Do not open Finder window on mounted disk images? (y/n)"
+  case $response in
+    [yY])
+	  defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool false
+	  defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool false
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Show icons for hard drives, servers, and removable media on the desktop? (y/n)"
+  case $response in
+    [yY])
+      defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Show hidden files in Finder by default? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.Finder AppleShowAllFiles -bool true
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Show dotfiles in Finder by default? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.finder AppleShowAllFiles TRUE
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Show all filename extensions in Finder by default? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Show status bar in Finder by default? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.finder ShowStatusBar -bool true
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Display full POSIX path as Finder window title? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Disable the warning when changing a file extension? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Use column view in all Finder windows by default? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.finder FXPreferredViewStyle Clmv
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Avoid creation of .DS_Store files on network volumes? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Disable disk image verification? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.frameworks.diskimages skip-verify -bool true
+      defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+      defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Enable airdrop on all network interfaces? (y/n)"
+  read -r response
+  case $response in
+      defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+      break;;
+      *) break;;
+  esac
+
+  # echo ""
+  # echo "Allowing text selection in Quick Look/Preview in Finder by default"
+  # defaults write com.apple.finder QLEnableTextSelection -bool true
+
+  # echo ""
+  # echo "Enabling snap-to-grid for icons on the desktop and in other icon views"
+  # /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+  # /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+  # /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+  ###############################################################################
+  # Dock & Mission Control
+  ###############################################################################
+  # echo ""
+  # echo "Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+  # defaults write com.apple.dock tilesize -int 36
+
+  # echo ""
+  # echo "Speeding up Mission Control animations and grouping windows by application"
+  # defaults write com.apple.dock expose-animation-duration -float 0.1
+  # defaults write com.apple.dock "expose-group-by-app" -bool true
+
+  echo ""
+  echo "Set Dock to auto-hide and remove the auto-hiding delay? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.dock autohide -bool true
+      defaults write com.apple.dock autohide-delay -float 0
+      defaults write com.apple.dock autohide-time-modifier -float 0
+      break;;
+    *) break;;
+  esac
+
+  ###############################################################################
+  # Mail
+  ###############################################################################
+  echo ""
+  echo "Set email addresses to copy as 'foo@example.com' instead of 'Foo Bar <foo@example.com>' in Mail.app? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+	  defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+    break;;
+    *) break;;
+  esac
+  ###############################################################################
+  # Messages                                                                    #
+  ###############################################################################
+  echo ""
+  echo "Disable automatic emoji substitution in Messages.app? (i.e. use plain text smileys) (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Disable smart quotes in Messages.app? (it's annoying for messages that contain code) (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
+      break;;
+    *) break;;
+  esac
+
+  echo ""
+  echo "Disable continuous spell checking in Messages.app? (y/n)"
+  read -r response
+  case $response in
+    [yY])
+      defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
+      break;;
+    *) break;;
+  esac
+
+  #################################################################################
+  # Power Saving Setting
+  #################################################################################
+  echo ""
+  echo "Disable restore on logout, poweroff, reboot by unchecking the checkbox? (y/n)"
+  read -r response
+  case $response in
+      # Disable restore on logout, poweroff, reboot by unchecking the checkbox
+      defaults write com.apple.loginwindow TALLogoutSavesState -bool false
+      defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
+      # Disable sleep from hitting powerbutton cause I do all the time cause it ablve
+      # delete: http://hints.macworld.com/article.php?story=20140305140635280
+      defaults write com.apple.loginwindow PowerButtonSleepsSystem -bool no
+      break;;
+    *) break;;
+  esac
+
+  #################################################################################
+  # Disables resume all apps
+  #################################################################################
+  echo ""
+  echo "Disable resume of Preview, Quicktime, TextEdit, Safari apps and system-wide? (y/n)"
+  read -r response
+  case $response in
+      #Disable resume system-wide.
+      defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+      # Disable restore on apps.
+      defaults write com.apple.Preview NSQuitAlwaysKeepsWindows -bool false
+      defaults write com.apple.QuickTimePlayerX NSQuitAlwaysKeepsWindows -bool false
+      defaults write com.apple.TextEdit NSQuitAlwaysKeepsWindows -bool false
+      defaults write com.apple.Safari NSQuitAlwaysKeepsWindows -bool false
+      break;;
+    *) break;;
+  esac
+
+  ###############################################################################
+  # Kill affected applications
+  ###############################################################################
+  echo ""
+  cecho "Done!" $cyan
+  echo ""
+  echo ""
+  cecho "################################################################################" $white
+  echo ""
+  echo ""
+  cecho "Note that some of these changes require a logout/restart to take effect." $red
+  cecho "Killing some open applications in order to take effect." $red
+  echo ""
+
+  find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
+  for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+    "Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer" \
+    "Terminal"; do
+    killall "${app}" > /dev/null 2>&1
+  done
+
+  # End
 fi
 exit;

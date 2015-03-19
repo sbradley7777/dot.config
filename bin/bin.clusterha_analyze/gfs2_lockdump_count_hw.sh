cd1bd6c62ac31a607c584dd8d04ac49349798c3c
Shane Bradley <shanebradley@gmail.com>
2015-03-19 13:13:45 -0400

    Added getopts and option to specify the min waiter/holder count.

--- bin/bin.clusterha_analyze/gfs2_lockdump_count_hw.sh
+++ bin/bin.clusterha_analyze/gfs2_lockdump_count_hw.sh
@@ -2,36 +2,82 @@
 # This script counts the number of holders and waiters for a GFS2 lockdump or
 # glocktop output.
 #
-# Usage: ./gfs2_lockdump_count_hw.sh <path to GFS2 lockdump or glocktop file>
+# Usage: ./gfs2_lockdump_count_hw.sh -m <minimum number of waiters/holder> -p <path to GFS2 lockdump or glocktop file>
 #
 #
 # TODO:
-# * Add getopts to give option to see count minimum for holder/waiter on
-#   glock.
 # * Add variable to hold name of GFS2 filesystem when analyzing glocktop
 #   output. Append to end of print line of glock.
 
-if [ -z $1 ]; then
-   echo "ERROR: A path to the file that will be analyzed is required.";
-   echo "Usage: $0 <path to GFS2 lockdump or glocktop file>";
+bname=$(basename $0);
+usage()
+{
+cat <<EOF
+usage: $bname -m <minimum number of waiters/holder> -p <path to GFS2 lockdump or glocktop file>
+
+This script counts the number of holders and waiters for a GFS2 lockdump or glocktop output.
+
+OPTIONS:
+   -h      Show this message
+   -m      Minimum number of waiters/holder that a glock has.
+   -p      Path to the file that will be analyzed.
+
+EXAMPLE:
+$ $bname -m 5 -p ~/myGFS2.glocks
+
+EOF
+}
+
+path_to_file="";
+minimum_hw=2;
+while getopts ":hm:p:" opt; do
+    case $opt in
+	h)
+	    usage;
+	    exit;
+	    ;;
+	m)
+	    minimum_hw=$OPTARG;
+	    ;;
+	p)
+	    path_to_file=$OPTARG;
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    exit 1
+	    ;;
+	:)
+	    echo "Option -$OPTARG requires an argument." >&2
+	    exit 1
+	    ;;
+    esac
+done
+
+# Make sure a path to lockdumps was given.
+if [ -z $path_to_file ]; then
+   echo "ERROR: A path to the file that will be analyzed is required with -p option.";
+   usage;
    exit 1
 fi
-if [ ! -f "$1" ]; then
+if [ ! -n $path_to_file ]; then
+   echo "ERROR: A path to the file that will be analyzed is required with the -p option.";
+   usage;
+   exit 1
+fi
+if [ ! -f $path_to_file ]; then
     echo "ERROR: The file does not exist: $1";
-    echo "Usage: $0 <path to GFS2 lockdump or glocktop file>";
+    usage;
     exit 1
 fi
-
 # hw_count is the holder/waiter count.
 hw_count=0;
 current_glock="";
-found_holder=1;
 current_holder="";
 while read line;do
     if [[ $line == G:* ]]; then
-	if [ "$hw_count" -gt "1" ]; then
+	if (( $hw_count >= $minimum_hw )); then
 	    printf -v hc "%03d" $hw_count;
-	    if [ "$found_holder" -eq "0" ]; then
+	    if [ -n "$current_holder" ]; then
 		echo "$hc ---> $current_glock (Has Holder: $current_holder)";
 	    else
 		echo "$hc ---> $current_glock";
@@ -39,15 +85,13 @@ while read line;do
 	fi
 	current_glock=$line;
 	hw_count=0;
-	found_holder=1;
 	current_holder="";
     elif [[ $line == *H:* ]]; then
 	((hw_count++));
 	# f:AH|f:H|f:EH
 	if [[ $line == *f:H* ]]; then
-	    found_holder=0;
 	    current_holder=$line;
 	fi
     fi
-done < $1
+done < $path_to_file;
 exit;

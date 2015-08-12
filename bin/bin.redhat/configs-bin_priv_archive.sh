5ced7e08f4248d5a35a57b30ec7dcfebcf8db27c
Shane Bradley <shanebradley@gmail.com>
2015-08-12 13:33:05 -0400

    Added new script to copy private configs to local dir.
    Signed-off-by: Shane Bradley <shanebradley@gmail.com>

---
+++ bin/bin.redhat/configs-bin_priv_archive.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# Author: sbradley@redhat.com
+# Description: The scripts uses /etc/hosts to get a list of hosts (that match a
+# regex) that will archive the ~/bin/bin.priv directory or any *.priv config 
+# file for each of the remote  hosts.
+# Version: 1.0
+#
+# Usage: ./configs-bin_priv_archive.sh
+
+# Current data for unique backups
+TODAY=`date '+%F_%s'`;
+# The path to directory that will contain archived files.
+ARCHIVE_DIR="/tmp/`date '+%F_%s'`-bin.priv";
+# The files that will be archived.
+files=( "~/.dot.config" "/root/.bash_profile.priv" "/root/.bashrc.priv" "/root/bin/bin.priv" );
+
+# Script will exit if this function is executed.
+control_c() {
+  # Run if user hits control-c so clean exit.
+  echo -en "\n*** Control-c caught and script will exit.***\n"
+  exit 1
+}
+# Trap keyboard interrupt (control-c)
+trap control_c SIGINT
+
+echo "The files will be archived to directory: $ARCHIVE_DIR";
+# Read /etc/hosts and do action on the remote hosts.
+while read -r line
+do
+    # Only hostnames with starting with "rh" and does not include "alt" will
+    # have files uploaded to them.
+    hosti=`echo $line | grep " rh" | grep -v "alt" | awk '{print $3}'`;
+    if [ ! -z $hosti ]; then
+        ping -q -c 1 $hosti &>/dev/null
+        if [ $? -eq 0 ] ; then
+    	    echo "Archiving the private files for the host: $hosti.";
+	    dst_dir=$ARCHIVE_DIR/$hosti;
+	    mkdir -p $dst_dir 2>/dev/null;
+	    if [ -d $ARCHIVE_DIR ]; then
+		for j in ${!files[@]}; do
+		    src=${files[j]};
+                    # Return 0 if file exists and 1 if files does not exist.
+		    #file_exists=$(ssh $hosti "test -e $src && echo 0 || echo 1")
+		    # if [[ "$file_exists" -eq "0" ]]; then
+		    scp -q -r root@$hosti:$src $dst_dir/ 2>/dev/null;
+		    if [[ ! "$?" -eq "0" ]]; then
+			echo "ERROR: There was an archiving the file $src on $hosti.";
+		    fi
+		   # fi
+		done
+	    fi
+        else
+            echo "WARNING: The host $hosti is unreachable. Skipping.";
+        fi
+    fi
+done < "/etc/hosts"
+parent_dir=$(dirname $ARCHIVE_DIR);
+path_to_tarball=$parent_dir/priv-$TODAY.tar.bz2;
+echo "Creating a tarball of the archived files: $path_to_tarball";
+tar jcf $path_to_tarball -C $ARCHIVE_DIR . 2>/dev/null;
+exit;
+
+

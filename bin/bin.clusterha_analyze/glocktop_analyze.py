f371385f3ec93dc89d29b2a8557970b5813bddf8
Shane Bradley <shanebradley@gmail.com>
2016-02-03 10:13:59 -0500

    Added new script to analyze glocktop output.
    Signed-off-by: Shane Bradley <shanebradley@gmail.com>

---
+++ bin/bin.clusterha_analyze/glocktop_analyze.py
@@ -0,0 +1,349 @@
+#!/usr/bin/python
+"""
+
+@author    : Shane Bradley
+@contact   : sbradley@redhat.com
+@version   : 0.1
+@copyright : GPLv2
+"""
+import sys
+import logging
+import logging.handlers
+import os
+import os.path
+from optparse import OptionParser, Option, SUPPRESS_HELP
+
+from datetime import datetime
+import re
+import calendar
+
+
+# #####################################################################
+# Global vars:
+# #####################################################################
+VERSION_NUMBER = "0.1-1"
+MAIN_LOGGER_NAME = "%s" %(os.path.basename(sys.argv[0]))
+
+# #####################################################################
+# Classes
+# #####################################################################
+
+class GFS2FilesystemSnapshot():
+    # A collection of glocks for a filesystem at a specific time.
+    def __init__(self, filesystem_name, hostname, date_time):
+        self.__filesystem_name = filesystem_name
+        self.__hostname = hostname
+        self.__date_time = date_time
+
+    def __str__(self):
+        return "%s - %s %s" %(self.get_filesystem_name(), str(self.get_date_time()), self.get_hostname())
+
+    def get_filesystem_name(self):
+        return self.__filesystem_name
+
+    def get_hostname(self):
+        return self.__hostname
+
+    def get_date_time(self):
+        return self.__date_time
+
+class Glock():
+    def __init__(self):
+        pass
+
+# #####################################################################
+# Parsers
+# #####################################################################
+def parse_glock(line):
+    #G:  s:EX n:2/181ec f:yIqob t:EX d:EX/0 a:1 v:0 r:3 m:200 (unknowninode)
+    regex = re.compile("^G:  s:(?P<state>\S+) n:(?P<type>\d)/(?P<inodeNumber>\S+) f:(?P<flags>\S*) t:(?P<target>\S+) d:(?P<demote_state>\S+)/(?P<demote_time>\d+)( l:(?P<lvbs>\d+))? a:(?P<ails>\d+)( v:(?P<v>\d+))? r:(?P<refs>\d+)( m:(?P<hold>\d+)) \((?P<glock_type>.*)\)")
+    mo = regex.match(line)
+    if mo:
+        # inodeNumber is hexadecimal.
+        return line
+        #return GFS2Glock(line, mo.group("state"), int(mo.group("type")),
+        #                 int(mo.group("inodeNumber"), 16), mo.group("flags"),
+        #                 mo.group("target"), mo.group("demote_state"),
+        #                 mo.group("demote_time"), mo.group("ails"), mo.group("refs"))
+    return None
+    parse_glock = staticmethod(parse_glock)
+
+# #####################################################################
+# Helper File Functions
+# ####################################################################
+
+def write_to_file(path_to_filename, data, append_to_file=True, create_file=False):
+    [parent_dir, filename] = os.path.split(path_to_filename)
+    if (os.path.isfile(path_to_filename) or (os.path.isdir(parent_dir) and create_file)):
+        try:
+            file_mode = "w"
+            if (append_to_file):
+                file_mode = "a"
+            fout = open(path_to_filename, file_mode)
+            fout.write(data + "\n")
+            fout.close()
+            return True
+        except UnicodeEncodeError, e:
+            message = "There was a unicode encode error writing to the file: %s." %(path_to_filename)
+            logging.getLogger(MAIN_LOGGER_NAME).error(message)
+            return False
+        except IOError:
+            message = "There was an error writing to the file: %s." %(path_to_filename)
+            logging.getLogger(MAIN_LOGGER_NAME).error(message)
+            return False
+    return False
+
+def mkdirs(path_to_dir):
+    if (os.path.isdir(path_to_dir)):
+        return True
+    elif ((not os.access(path_to_dir, os.F_OK)) and (len(path_to_dir) > 0)):
+        try:
+            os.makedirs(path_to_dir)
+        except (OSError, os.error):
+            message = "Could not create the directory: %s." %(path_to_dir)
+            logging.getLogger(MAIN_LOGGER_NAME).error(message)
+            return False
+        except (IOError, os.error):
+            message = "Could not create the directory with the path: %s." %(path_to_dir)
+            logging.getLogger(MAIN_LOGGER_NAME).error(message)
+            return False
+    return os.path.isdir(path_to_dir)
+
+def get_data_from_file(path_to_filename) :
+    if (len(path_to_filename) > 0) :
+        try:
+            fin = open(path_to_filename, "r")
+            data = fin.readlines()
+            fin.close()
+            mod_data = []
+            for line in data:
+                mod_data.append(line.strip())
+            return mod_data
+        except (IOError, os.error):
+            message = "An error occured reading the file: %s." %(path_to_filename)
+            logging.getLogger(MAIN_LOGGER_NAME).error(message)
+    return []
+
+# ##############################################################################
+# Get user selected options
+# ##############################################################################
+def __get_options(version) :
+    cmd_parser = OptionParserExtended(version)
+    cmd_parser.add_option("-d", "--debug",
+                         action="store_true",
+                         dest="enableDebugLogging",
+                         help="enables debug logging",
+                         default=False)
+    cmd_parser.add_option("-q", "--quiet",
+                         action="store_true",
+                         dest="disableLoggingToConsole",
+                         help="disables logging to console",
+                         default=False)
+    cmd_parser.add_option("-y", "--no_ask",
+                        action="store_true",
+                         dest="disableQuestions",
+                         help="disables all questions and assumes yes",
+                         default=False)
+    cmd_parser.add_option("-p", "--path_to_filename",
+                         action="store",
+                         dest="path_to_src_file",
+                         help="the path to the filename that will be parsed",
+                         type="string",
+                         metavar="<input filename>",
+                         default="")
+    cmd_parser.add_option("-m", "--minimum_waiter_count",
+                          action="store",
+                          dest="minimum_waiter_count",
+                          help="the minimum number of waiters for a glock",
+                          type="int",
+                          metavar="<minimum waiter count>",
+                          default=2)
+    cmd_parser.add_option("-S", "--no_glock_summary",
+                          action="store_true",
+                          dest="no_glock_summary",
+                          help="the glock summary is not displayed",
+                          default=False)
+    cmd_parser.add_option("-w", "--show_waiters",
+                          action="store_true",
+                          dest="show waiters",
+                          help="the waiters are displayed",
+                          default=False)
+    cmd_parser.add_option("-g", "--find_glock",
+                          action="store",
+                          dest="glock_number",
+                          help="a glock hexadecimal number to search for",
+                          type="string",
+                          metavar="0x<glock number>",
+                          default="")
+    cmd_parser.add_option("-G", "--find_glock_type",
+                          action="store",
+                          dest="glock_type",
+                          help="a glock type to search for",
+                          type="int",
+                          metavar="<glock type>",
+                          default=None)
+
+ # Get the options and return the result.
+    (cmdLine_opts, cmdLine_args) = cmd_parser.parse_args()
+    return (cmdLine_opts, cmdLine_args)
+
+# ##############################################################################
+# OptParse classes for commandline options
+# ##############################################################################
+class OptionParserExtended(OptionParser):
+    def __init__(self, version) :
+        self.__command_name = os.path.basename(sys.argv[0])
+        OptionParser.__init__(self, option_class=ExtendOption,
+                              version="%s %s\n" %(self.__command_name, version),
+                              description="%s \n"%(self.__command_name))
+
+    def print_help(self):
+        self.print_version()
+        examples_message = "\n"
+        OptionParser.print_help(self)
+        #print examples_message
+
+class ExtendOption (Option):
+    ACTIONS = Option.ACTIONS + ("extend",)
+    STORE_ACTIONS = Option.STORE_ACTIONS + ("extend",)
+    TYPED_ACTIONS = Option.TYPED_ACTIONS + ("extend",)
+
+    def take_action(self, action, dest, opt, value, values, parser):
+        if (action == "extend") :
+            valueList = []
+            try:
+                for v in value.split(","):
+                    # Need to add code for dealing with paths if there is option for paths.
+                    newValue = value.strip().rstrip()
+                    if (len(newValue) > 0):
+                        valueList.append(newValue)
+            except:
+                pass
+            else:
+                values.ensure_value(dest, []).extend(valueList)
+        else:
+            Option.take_action(self, action, dest, opt, value, values, parser)
+
+# ###############################################################################
+# Main Function
+# ###############################################################################
+if __name__ == "__main__":
+    try:
+        # #######################################################################
+        # Get the options from the commandline.
+        # #######################################################################
+        (cmdline_opts, cmdline_args) = __get_options(VERSION_NUMBER)
+        # #######################################################################
+        # Setup the logger and create config directory
+        # #######################################################################
+        # Create the logger
+        logLevel = logging.INFO
+        logger = logging.getLogger(MAIN_LOGGER_NAME)
+        logger.setLevel(logLevel)
+        # Create a new status function and level.
+        logging.STATUS = logging.INFO + 2
+        logging.addLevelName(logging.STATUS, "STATUS")
+
+        # Log to main system logger that script has started then close the
+        # handler before the other handlers are created.
+        sysLogHandler = logging.handlers.SysLogHandler(address = '/dev/log')
+        logger.addHandler(sysLogHandler)
+        logger.info("The script has started running.")
+        logger.removeHandler(sysLogHandler)
+
+        # Create a function for the STATUS_LEVEL since not defined by python. This
+        # means you can call it like the other predefined message
+        # functions. Example: logging.getLogger("loggerName").status(message)
+        setattr(logger, "status", lambda *args: logger.log(logging.STATUS, *args))
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logLevel)
+        stream_handler.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
+        logger.addHandler(stream_handler)
+
+        # #######################################################################
+        # Set the logging levels.
+        # #######################################################################
+        if ((cmdline_opts.enableDebugLogging) and (not cmdline_opts.disableLoggingToConsole)):
+            logging.getLogger(MAIN_LOGGER_NAME).setLevel(logging.DEBUG)
+            stream_handler.setLevel(logging.DEBUG)
+            message = "Debugging has been enabled."
+            logging.getLogger(MAIN_LOGGER_NAME).debug(message)
+        if (cmdline_opts.disableLoggingToConsole):
+            stream_handler.setLevel(logging.CRITICAL)
+
+
+        # #######################################################################
+        # Validate input
+        # #######################################################################
+        if (not cmdline_opts.path_to_src_file):
+            logging.getLogger(MAIN_LOGGER_NAME).error("A path to a file (-p) to be analyzed is required.")
+            sys.exit(1)
+        if (not os.path.exists(cmdline_opts.path_to_src_file)):
+            message ="The file does not exist: %s" %(cmdline_opts.path_to_src_file)
+            logging.getLogger(MAIN_LOGGER_NAME).error(message)
+            sys.exit(1)
+        if (not cmdline_opts.glock_type == None):
+            if (not (0 < cmdline_opts.glock_type < 10)):
+                logging.getLogger(MAIN_LOGGER_NAME).error("The glock type (-G) must be an integer between 1 - 9.")
+                sys.exit(1)
+
+        glock_number = ""
+        if (cmdline_opts.glock_number):
+            try:
+                if (cmdline_opts.glock_number.startswith("0x")):
+                    glock_number = int("%s" %(cmdline_opts.glock_number), 16)
+                else:
+                    glock_number = int("0x%s" %(cmdline_opts.glock_number), 16)
+            except ValueError:
+                logging.getLogger(MAIN_LOGGER_NAME).error("The glock number (-g) must be a hexadecimal number.")
+                sys.exit(1)
+            except TypeError:
+                logging.getLogger(MAIN_LOGGER_NAME).error("The glock number (-g) must be a hexadecimal number.")
+                sys.exit(1)
+
+        # #######################################################################
+        # Run main
+        # #######################################################################
+
+
+
+
+        message ="The file will be analyzed: %s" %(cmdline_opts.path_to_src_file)
+        logging.getLogger(MAIN_LOGGER_NAME).info(message)
+        # Get the data as a list of lines
+        lines = get_data_from_file(cmdline_opts.path_to_src_file)
+
+        # @ nate_bob1       Mon Feb  1 15:04:11 2016  @host-050.virt.lab.msp.redhat.com
+
+        #date_expr = r"\d{2} (?:%s) \d{4}" % '|'.join(calendar.month_abbr[1:])
+        days = "(?P<day>%s)" % '|'.join(calendar.day_abbr[0:])
+        months = "(?P<month>%s)" % '|'.join(calendar.month_abbr[1:])
+        dow = "(?P<dow>\d{1,2})"
+        time = "(?P<time>\d{1,2}:\d\d:\d\d)"
+        year = "(?P<year>\d{4})"
+        hostname = "@(?P<hostname>.*)"
+        regex = "^@ (?P<filesystem>\w+)\s+%s\s%s\s*%s\s%s\s%s\s\s%s" %(days, months, dow, time, year, hostname)
+
+        glocks = []
+        for line in lines:
+            if (line.startswith("@")):
+                rem = re.compile(regex)
+                mo = rem.match(line)
+                if mo:
+                    date_time = datetime.strptime("%s %s %s %s" %(mo.group("month"), mo.group("dow"), mo.group("year"), mo.group("time")), "%b %d %Y %H:%M:%S")
+                    gfs2_snapshot = GFS2FilesystemSnapshot(mo.group("filesystem"),  mo.group("hostname"), date_time)
+                    print gfs2_snapshot
+            if (line.startswith("G")):
+                print parse_glock(line)
+        # I could split these sections up into time slices + filesystem. Kind of group them together.
+        # Then take each section and analyze that section
+
+    except KeyboardInterrupt:
+        print ""
+        message =  "This script will exit since control-c was executed by end user."
+        logging.getLogger(MAIN_LOGGER_NAME).error(message)
+        sys.exit(1)
+    # #######################################################################
+    # Exit the application with zero exit code since we cleanly exited.
+    # #######################################################################
+    sys.exit()

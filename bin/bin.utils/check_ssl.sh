265dace634afe6d32e3b6913910a2246aec048d2
Shane Bradley <shanebradley@gmail.com>
2016-04-27 11:30:29 -0400

    Added tls to list to check.
    Signed-off-by: Shane Bradley <shanebradley@gmail.com>

--- bin/bin.utils/check_ssl.sh
+++ bin/bin.utils/check_ssl.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
-# Detect which version of SSL that can be connected to.
-# Based on poodle.sh
+# Detect which version of SSL that can be connected to. Defaults for IP is
+# 127.0.0.1 and port is 8084 if no options are specified on command line.
+#
+# usage:
+# ./check_ssl.sh <ip> <port>
+# Example:
+# ./check_ssl.sh 192.168.122.161 8084
 
 host=${1:-127.0.0.1};
 port=${2:-8084};
 timeout_bin=`which timeout 2>/dev/null`;
 
-for ssl_version in "ssl2" "ssl3" ; do
-    out="`echo 'Q' | ${timeout_bin:+$timeout_bin 5} openssl s_client -$ssl_version -connect "${host}:${port}" 2>/dev/null`"
+for protocal in "ssl2" "ssl3" "tls1_1" "tls1_2"; do
+    out="`echo 'Q' | ${timeout_bin:+$timeout_bin 5} openssl s_client -$protocal -connect "${host}:${port}" 2>/dev/null`"
     if [ $? -eq 124 ]; then
-	echo "ERROR: Timeout connecting to \"$host:$port\" to check SSL version: $ssl_version.";
+	echo "ERROR: Timeout connecting to \"$host:$port\" to check protocal: $protocal.";
     else
 	connected=`echo "$out" | grep '^CONNECTED'`;
 	if [ $? -eq 0 ]; then
 	    proto=`echo "$out" | grep '^ *Protocol *:' | awk '{ print $3 }'`;
 	    cipher=`echo "$out" | grep '^ *Cipher *:' | awk '{ print $3 }'`;
-	    echo "$host:$port | SSL version: $proto Cipher: $cipher";
+	    echo "$host:$port | Protocal: $proto Cipher: $cipher";
 	    if [ "$cipher" = '0000'  -o  "$cipher" = '(NONE)' ]; then
-		echo "  Connected with \""${ssl_version^^}"\" but no ciphers were found.";
+		echo "  Connected with \""${protocal^^}"\" but no ciphers were found.";
 	    else
-		echo "  Connected with \""${ssl_version^^}"\" and using cipher: $cipher.";
+		echo "  Connected with \""${protocal^^}"\" and using cipher: $cipher.";
 	    fi
 	else
-	    echo "$host:$port | SSL version: ${ssl_version^^}";
-	    echo "  Failed to establish a \""${ssl_version^^}"\" connection.";
+	    echo "$host:$port | Protocal: ${protocal^^}";
+	    echo "  Failed to establish a \""${protocal^^}"\" connection.";
 	fi
+	echo "";
     fi
 done
 exit;

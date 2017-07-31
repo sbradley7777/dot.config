1d051935626b0359465c633750f6203db779a56e
Shane Bradley <shanebradley@gmail.com>
2017-07-31 10:30:14 -0400

    Only return first result.
    Signed-off-by: Shane Bradley <shanebradley@gmail.com>

--- bin/bin.utils/getdstip.sh
+++ bin/bin.utils/getdstip.sh
@@ -8,7 +8,7 @@ host=$1;
 ping -q -c 1 $host &>/dev/null
 if [ $? -eq 0 ] ; then
     #ipAddr=$(ssh $host  "/sbin/ifconfig $ETHERNET_DEVICE | grep 'inet addr:' | cut -d: -f2 " | awk '{ print $1}');
-    ipAddr=$(ssh $host  "ip addr show dev $ETHERNET_DEVICE | sed -e's/^.*inet \([^ ]*\)\/.*$/\1/;t;d' 2>/dev/null");
+    ipAddr=$(ssh -o ForwardX11=no $host  "ip addr show dev $ETHERNET_DEVICE | sed -e's/^.*inet \([^ ]*\)\/.*$/\1/;t;d' 2>/dev/null" | head -n 1);
     echo "$ipAddr";
     exit;
 else

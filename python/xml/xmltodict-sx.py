ae7d5ad562b9c56d06bd6ad99ef571cb56adbc26
Shane Bradley <shanebradley@gmail.com>
2017-11-03 11:21:08 -0400

    Updated xml example.
    Signed-off-by: Shane Bradley <shanebradley@gmail.com>

--- python/xml/xmltodict-sx.py
+++ python/xml/xmltodict-sx.py
@@ -7,46 +7,23 @@ I found this code at: http://code.activestate.com/recipes/410469-xml-as-dictiona
 """
 import sys
 import os.path
-from sxstrata import XmlToDict
+from xml.etree.ElementTree import ElementTree
 
+def parse_xml_file(path_to_xml_file):
 
-def printXML(pathToXmlFile) :
-    if (not os.path.isfile(pathToXmlFile)):
-        print "Error: The xml file does not exist: %s" %(pathToXmlFile)
-        sys.exit()
-    # ##########################################################################
-    # If file exists then start parsing.
-    # ##########################################################################
-    xmlString = ""
-    try:
-        f = open(pathToXmlFile, "r")
-        lines = f.readlines()
-        f.close()
-        for line in lines:
-            xmlString += "%s" %(line)
-    except IOError:
-        print "Error: There was an error reading the file: %s." %(pathToXmlFile)
-
-    #print xmlString
-    #xmlDict = XmlToDict().xmlFileToDict(pathToXmlFile)
-    xmlDict = XmlToDict().xmlToDict(xmlString)
-
-    for key in  xmlDict.keys():
-        for fileList in xmlDict[key]:
-            print "ITEM: %s" %(str(fileList))
-            print
-
+    tree = ElementTree()
+    tree.parse(path_to_xml_file)
+    results = tree.findall(".//Name")
+    for item in results:
+        print item
+        print item.text
 
 ################################################################################
 #Run script
 ################################################################################
 if __name__ == "__main__":
-    pathToXmlFile = "cluster.conf"
-    printXML(pathToXmlFile)
-    print "---------------------------"
-    print ""
-    pathToXmlFile = "example.xml"
-    printXML(pathToXmlFile)
+    path_to_xml_file= "/home/sbradley/bin/bin.priv/example.xml"
+    parse_xml_file(path_to_xml_file)
     sys.exit()
 ################################################################################
 

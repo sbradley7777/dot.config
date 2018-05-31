52b064adb333d9c74d90dc7b8e55e30b1631b648
Shane Bradley <sbradley@redhat.com>
2018-05-31 10:00:54 -0400

    Fixed finding ip.
    Signed-off-by: Shane Bradley <sbradley@redhat.com>

--- bin/bin.utils/getdstip.sh
+++ bin/bin.utils/getdstip.sh
@@ -2,14 +2,40 @@
 
 # This script will get the ip for an ethernet device for some host.
 
-ETHERNET_DEVICE="eth0";
+
+function valid_ip() {
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+
 
 host=$1;
 ping -q -c 1 $host &>/dev/null
 if [ $? -eq 0 ] ; then
     #ipAddr=$(ssh $host  "/sbin/ifconfig $ETHERNET_DEVICE | grep 'inet addr:' | cut -d: -f2 " | awk '{ print $1}');
-    ipAddr=$(ssh -o ForwardX11=no $host  "ip addr show dev $ETHERNET_DEVICE | sed -e's/^.*inet \([^ ]*\)\/.*$/\1/;t;d' 2>/dev/null" | head -n 1);
-    echo "$ipAddr";
+    ethernet_device="eth0";
+    ipAddr=$(ssh -o ForwardX11=no $host  "ip addr show dev $ethernet_device 2>/dev/null | sed -e's/^.*inet \([^ ]*\)\/.*$/\1/;t;d' 2>/dev/null" | head -n 1);
+    if valid_ip $ipAddr; then
+	echo "$ipAddr"
+    else
+	ethernet_device="ens3";
+	ipAddr=$(ssh -o ForwardX11=no $host  "ip addr show dev $ethernet_device 2>/dev/null | sed -e's/^.*inet \([^ ]*\)\/.*$/\1/;t;d' 2>/dev/null" | head -n 1);
+	if valid_ip $ipAddr; then
+	    echo "$ipAddr"
+	fi
+    fi;
     exit;
 else
     echo "";

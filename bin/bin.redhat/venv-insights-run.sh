d2661ff33e40a4cbad055ad98e786f89654dc75a
Shane Bradley <sbradley@redhat.com>
2020-01-29 12:03:05 -0500

    Added some error checking.
    Signed-off-by: Shane Bradley <sbradley@redhat.com>

--- bin/bin.redhat/venv-insights-run.sh
+++ bin/bin.redhat/venv-insights-run.sh
@@ -8,8 +8,23 @@ if [ -z $1 ]; then
 fi
 path_to_sosreport=$1;
 
+if [ ! -d $PATH_TO_INSIGHTS_PLUGINS ]; then
+    echo "The path to the insights-plugins directory does not exist: $PATH_TO_INSIGHTS_PLUGINS";
+    exit 1;
+fi
+
+if [ ! -f $PATH_TO_INSIGHTS_PLUGINS/bin/activate ]; then
+    echo "ERROR: Missing script: activate. The insights-plugins directory has not been initialized with virtualenv: $PATH_TO_INSIGHTS_PLUGINS.";
+    exit 1;
+fi
+
+if [ ! -f $PATH_TO_INSIGHTS_PLUGINS/bin/insights-run ]; then
+    echo "ERROR: Missing command: insights-run. The insights-plugins directory has not been initialized with virtualenv and installed all the requirements: $PATH_TO_INSIGHTS_PLUGINS.";
+    exit 1;
+fi
+
 cd $PATH_TO_INSIGHTS_PLUGINS;
 source $PATH_TO_INSIGHTS_PLUGINS/bin/activate;
-insights-run -p telemetry,shared_rules $path_to_sosreport
+insights-run -p telemetry,shared_rules $path_to_sosreport;
 exit;
 

bcea05a8a7569eaf89d46c57de16f3b3391fce6c
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2024-07-08 15:48:44 -0400

    Update theme use and downloading packages.
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

--- conf/.emacs.d/dot.emacs.el
+++ conf/.emacs.d/dot.emacs.el
@@ -5,12 +5,12 @@
 (message "Loading package management and MELPA repository.")
 (require 'package)
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 ;; The init will load all the packages into the load path.
 (package-initialize)
-
 (add-to-list 'package-pinned-packages '("gnu-elpa-keyring-update" . "gnu"))
-
 ;; Install or update the key required for "melpa".
 ;;   - https://stackoverflow.com/questions/5701388/where-can-i-find-the-public-key-for-gnu-emacs
 (unless (package-installed-p 'gnu-elpa-keyring-update)
@@ -24,6 +24,33 @@
   (package-install 'gnu-elpa-keyring-update t)
   ;; Restore `package-check-signature' value to default.
   (setq package-check-signature package-check-signature-default))
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Install and load packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; myPackages contains a list of package names
+(defvar myPackages
+  '(spacemacs-theme ;; Theme
+    )
+  )
+
+;; Scans the list in myPackages and if the package listed is not already installed, then install it.
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      myPackages)
+
+;; Customize theme.
+;;   - https://melpa.org/#/?q=theme&sort=downloads&asc=false
+;;   - https://github.com/nashamri/spacemacs-theme?tab=readme-ov-file#override-themes-colors
+(custom-set-variables '(spacemacs-theme-custom-colors
+      '(
+        (bg1 . "#000000")
+	)))
+(load-theme 'spacemacs-dark t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load all the libraries under the directory: ~/.emacs.d/site-lisp
@@ -46,8 +73,8 @@
 (load-library "functions.el")
 ;; Load all misc preferences.
 (load-library "prefs.el")
-;; Load all theme preferences.
-(load-library "theme.el")
+;; Load all theme preferences. This is comment out because loading a packaged theme.
+;; (load-library "theme.el")
 ;; Load all mode preferences.
 (load-library "modes.el")
 

107de95cc56bc65c567c77cc86063e880a9a283b
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2024-07-23 14:54:50 -0400

    REmove -s option.
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

--- bin/bin.clusterha_analyze/glocktop_parse.sh
+++ bin/bin.clusterha_analyze/glocktop_parse.sh
@@ -8,7 +8,7 @@ usage()
 {
     script_name=$(basename $0);
     cat <<EOF
-usage: $script_name -p <path to the glocktop file> -n <filesystem name> -s <size of prefix spacing> -f -l
+usage: $script_name -p <path to the glocktop file> -n <filesystem name> -f -l
 
 This script will parse glocktop files and output formatted data.
 
@@ -16,7 +16,6 @@ OPTIONS:
    -h      show this message
    -p      path to the glocktop file that will be analyzed
    -n      name of the gfs2 filesystem
-   -s      size of prefix spacing that will be added to the parsed data (default: 0)
    -l      list the gfs2 filesystems in the glocktop file
    -f      show count of functions that processes are in
 
@@ -24,7 +23,7 @@ EXAMPLES:
   List the gfs2 filesystems in the glocktop file.
     $ $script_name -p <path to the glocktop file> -l
 
-  Parse the glocktop file and add prefix spacing of 2 (2 whitespaces are added to the start of each line)
+  Parse the glocktop file for particular filesystem name.
     $ $script_name -p <path to the glocktop file> -n <name of the gfs2 filesystem> -s 2
 
   List the count for each function that processes are in.
@@ -36,12 +35,11 @@ EOF
 # Option variables.
 path_to_file="";
 filesystem_name="";
-prefix_space_size=0;
 list_filesystems=1;
 show_function_count=1;
 
 # Parse the options.
-while getopts "hp:n:s:lf" opt; do
+while getopts "hp:n:lf" opt; do
     case $opt in
 	h)
 	    usage;
@@ -52,9 +50,6 @@ while getopts "hp:n:s:lf" opt; do
 	    ;;
 	n)
 	    filesystem_name=$OPTARG;
-	    ;;
-	s)
-	    prefix_space_size=$OPTARG;
 	    ;;
 	l)
 	    list_filesystems=0;
@@ -87,12 +82,6 @@ if [ -z $path_to_file ] || [ ! -n $path_to_file ]; then
    exit 1;
 elif [ ! -f $path_to_file ]; then
     echo "ERROR: The glocktop file does not exist: $path_to_file.";
-    exit 1;
-fi
-
-# Verify that prefix spacing is a number greater than zero.
-if [[ "$prefix_space_size" =~ ^[-+]?([0-9][[:digit:]]*)$ && "$prefix_space_size" -lt 0 ]]; then
-    echo "ERROR: The option -s is required to be greater than or equal to 0. The script will exit.";
     exit 1;
 fi
 
@@ -138,5 +127,5 @@ fi
 
 # Print the parsed data. The "sed" command will add a new line before each @ so that we have seperation for our
 # first "awk" command. The second awk command add prefix spacing to each line.
-sed s/"^@"/"\n@"/g $path_to_file | awk "/@ $filesystem_name/,/^$/" | grep -ie "@" -ie "G\:" -ie "H\: "| grep -v -ie "Held SH" -ie "S G Waiting" -ie "S P Waiting" -ie ended | awk -v prefix_spacing="$(printf "%*s" $prefix_space_size)" '{print prefix_spacing $0}';
+sed s/"^@"/"\n@"/g $path_to_file | awk "/@ $filesystem_name/,/^$/" | grep -ie "@" -ie "G\:" -ie "H\: "| grep -v -ie "Held SH" -ie "S G Waiting" -ie "S P Waiting" -ie ended;
 exit;

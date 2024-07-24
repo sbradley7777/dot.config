d893da62bc1e71c1bd9433e46077098b283d8d83
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2024-07-24 08:10:43 -0400

    Add new function, reorder function count command, add new future option for summary (-s) that will be added in future (currently does not work).
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

--- bin/bin.clusterha_analyze/glocktop_parse.sh
+++ bin/bin.clusterha_analyze/glocktop_parse.sh
@@ -8,7 +8,7 @@ usage()
 {
     script_name=$(basename $0);
     cat <<EOF
-usage: $script_name -p <path to the glocktop file> -n <filesystem name> -f -l
+usage: $script_name -p <path to the glocktop file> -n <filesystem name> -f -l -s
 
 This script will parse glocktop files and output formatted data.
 
@@ -18,6 +18,7 @@ OPTIONS:
    -n      name of the gfs2 filesystem
    -l      list the gfs2 filesystems in the glocktop file
    -f      show count of functions that processes are in
+   -s      summary of glocktop file
 
 EXAMPLES:
   List the gfs2 filesystems in the glocktop file.
@@ -29,6 +30,9 @@ EXAMPLES:
   List the count for each function that processes are in.
     $ $script_name -p <path to the glocktop file> -f
 
+  Show summary of glocktop file.
+    $ $script_name -p <path to the glocktop file> -s
+
 EOF
 }
 
@@ -37,9 +41,10 @@ path_to_file="";
 filesystem_name="";
 list_filesystems=1;
 show_function_count=1;
+show_summary=1;
 
 # Parse the options.
-while getopts "hp:n:lf" opt; do
+while getopts "hp:n:lfs" opt; do
     case $opt in
 	h)
 	    usage;
@@ -57,8 +62,15 @@ while getopts "hp:n:lf" opt; do
 	f)
 	    show_function_count=0;
 	    ;;
+	s)
+	    show_summary=0;
+	    ;;
     esac
 done
+
+show_glocks() {
+    sed s/"^@"/"\n@"/g $1 | awk "/@ $2/,/^$/" | grep -ie "@" -ie "G\:" -ie "H\: "| grep -v -ie "Held SH" -ie "S G Waiting" -ie "S P Waiting" -ie ended;
+}
 
 list_filesystems() {
    grep "@" $1 | awk '{print $2}' | sort | uniq;
@@ -71,10 +83,26 @@ show_function_count() {
     fi
     for fs_name in "${fs_names[@]}"; do
 	echo $fs_name;
-	sed s/"^@"/"\n@"/g $1 | grep -v -ie "Held SH" -ie "S G Waiting" -ie "S P Waiting" | awk " /@ $fs_name/,/^$/" | grep "H:" | cut -d "]" -f 2 | cut -d "[" -f 1 | sort | uniq -c | sort -rnk1;
-	echo -e "\n";
+	sed s/"^@"/"\n@"/g $1 | awk "/@ $fs_name/,/^$/" | grep "H:" | grep -v -ie "Held SH" -ie "S G Waiting" -ie "S P Waiting" | cut -d "]" -f 2 | cut -d "[" -f 1 | sort | uniq -c | sort -rnk1;
     done
 }
+
+summarize_filesystem() {
+    echo "Future feature";
+    echo $1;
+    echo $2;
+}
+
+show_summary() {
+    fs_names=( $2 )
+    if [ ${#fs_names[@]} -eq 0 ]; then
+        mapfile -t fs_names < <( list_filesystems $1 )
+    fi
+    for fs_name in "${fs_names[@]}"; do
+	summarize_filesystem $1 $fs_name;
+    done
+}
+
 
 # ####################################################################
 # Validate Options
@@ -91,7 +119,7 @@ fi
 # Verify that a filesystem name was given and it exists.
 if [ -z $filesystem_name ] || [ ! -n $filesystem_name ]; then
     # Do not error out if empty $filesystem_name when -l or -f option is enabled.
-    if (( $show_function_count != 0 )) && (( $list_filesystems != 0 )); then
+    if (( $show_function_count != 0 )) && (( $list_filesystems != 0 )) && (( $show_summary != 0 )); then
 	echo "ERROR: A filesystem name (-n) is required. The script will exit.";
 	exit 1;
     fi
@@ -129,7 +157,14 @@ if (( $show_function_count == 0 )); then
     exit;
 fi
 
+# Show summary of data within the glocktop file.
+if (( $show_summary == 0 )); then
+    show_summary $path_to_file $filesystem_name;
+    exit;
+fi
+
+
 # Print the parsed data. The "sed" command will add a new line before each @ so that we have seperation for our
 # first "awk" command. The second awk command add prefix spacing to each line.
-sed s/"^@"/"\n@"/g $path_to_file | awk "/@ $filesystem_name/,/^$/" | grep -ie "@" -ie "G\:" -ie "H\: "| grep -v -ie "Held SH" -ie "S G Waiting" -ie "S P Waiting" -ie ended;
+show_glocks $path_to_file $filesystem_name;
 exit;
